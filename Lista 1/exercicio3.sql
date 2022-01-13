90c58e0ce5906dd40deabde156463e5e5734dc1d
Edgar Soares <edemarinho@outlook.com>
2022-01-13 13:15:15 -0300

    Atualização lista 1

--- Lista 1/exercicio3.sql
+++ Lista 1/exercicio3.sql
@@ -10,22 +10,15 @@ CREATE TABLE alunos (
     media INT NOT NULL        
 );
 
-INSERT INTO alunos (nome, idade, telefone, media)
-VALUES ('Pedrinho', '8', '11943455566', '6');
-INSERT INTO alunos (nome, idade, telefone, media)
-VALUES ('Mariazinha', '7', '11943436633', '8');
-INSERT INTO alunos (nome, idade, telefone, media)
-VALUES ('Claudinho', '11', '11934456644', '8');
-INSERT INTO alunos (nome, idade, telefone, media)
-VALUES ('Borges', '7', '11946778990', '4');
-INSERT INTO alunos (nome, idade, telefone, media)
-VALUES ('Rodolfo', '8', '11966635367', '6');
-INSERT INTO alunos (nome, idade, telefone, media)
-VALUES ('Josefina', '10', '11946663772', '9');
-INSERT INTO alunos (nome, idade, telefone, media)
-VALUES ('Gertrudes', '9', '11962849874', '8');
-INSERT INTO alunos (nome, idade, telefone, media)
-VALUES ('Wesley', '8', '11973854566', '9');
+INSERT INTO alunos (nome, idade, telefone, media) VALUES 
+('Pedrinho', '8', '11943455566', '6'),
+('Mariazinha', '7', '11943436633', '8'),
+('Claudinho', '11', '11934456644', '8'),
+('Borges', '7', '11946778990', '4'),
+('Rodolfo', '8', '11966635367', '6'),
+('Josefina', '10', '11946663772', '9'),
+('Gertrudes', '9', '11962849874', '8'),
+('Wesley', '8', '11973854566', '9');
 
 SELECT * FROM alunos WHERE media > 7;
 
